c0f941427eeea70774f1284a83b485455e293e72
Tommy-S05 <tommy-s05f@hotmail.com>
2023-09-03 21:28:42 -0400

    modificando el seeder del usuario

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'Elber Galarga',
             'email' => 'elberg@hotmail.com',
             'username' => 'elberg',
         ]);
